2ea7e2556bb63866ff1f57eaf43fed8754e28515
carivash <carivash.dev@gmail.com>
2023-12-05 23:09:09 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Node, Svelvet, Minimap, Controls } from 'svelvet';
+</script>
+
+<Svelvet id="my-canvas" width="{500}" height="{500}" TD controls>
+  <Node />
+  <Node id="alpha" bgColor="red" label="Default Node" inputs="{3}" />
+</Svelvet>
